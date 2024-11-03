98fcc6ed02e2005c66785d1060c8b30b67ee84a2
GitLab Runner <gitlab.runner@build.iserv.eu>
2024-11-03 22:34:07 +0100

    Merge branch '76054-fix-extract-patches' into 'master'
    fixed overwriting of variables (fixes #76054)
    
    See merge request iserv/docker-cloudfiles!115

--- iserv-build/source/extract_patches.sh
+++ iserv-build/source/extract_patches.sh
@@ -26,31 +26,31 @@ rm -rf "$PATCHES_DIR"
 
 create_patches_for_repo()
 {
-  REPO_DIR="$1"
-  ADDED_DIR="$2"
-  PATCHES_DIR="$3"
+  REPO_DIR_LOCAL="$1"
+  ADDED_DIR_LOCAL="$2"
+  PATCHES_DIR_LOCAL="$3"
   PREFIX="$4"
-  for F in $(git -C "$REPO_DIR" ls-files --modified --other)
+  for F in $(git -C "$REPO_DIR_LOCAL" ls-files --modified --other)
   do
-    if [ -d "$REPO_DIR/$F" ] ; then
+    if [ -d "$REPO_DIR_LOCAL/$F" ] ; then
       # skip directories
       continue
     fi
-    DIFF=$(git -C "$REPO_DIR" diff "$F")
+    DIFF=$(git -C "$REPO_DIR_LOCAL" diff "$F")
     DIR=$(dirname "$F")
     if [ -z "$DIFF" ] ; then
       # Completely new file
       echo "New: $F"
-      mkdir -p "$ADDED_DIR/$DIR"
-      cp "$REPO_DIR/$F" "$ADDED_DIR/$F"
+      mkdir -p "$ADDED_DIR_LOCAL/$DIR"
+      cp "$REPO_DIR_LOCAL/$F" "$ADDED_DIR_LOCAL/$F"
     else
       # Modified file
       echo "Modified: $F"
-      if grep -q "^<<<<<<<$" "$REPO_DIR/$F" ; then
+      if grep -q "^<<<<<<<$" "$REPO_DIR_LOCAL/$F" ; then
         echo "${COLOR_WARNING}WARNING: It looks like $F contains an unresolved merge conflict!${COLOR_NEUTRAL}"
       fi
-      mkdir -p "$PATCHES_DIR/$DIR"
-      git -C "$REPO_DIR" diff --src-prefix="$PREFIX" --dst-prefix="$PREFIX" "$F" > "$PATCHES_DIR/$F.patch"
+      mkdir -p "$PATCHES_DIR_LOCAL/$DIR"
+      git -C "$REPO_DIR_LOCAL" diff --src-prefix="$PREFIX" --dst-prefix="$PREFIX" "$F" > "$PATCHES_DIR_LOCAL/$F.patch"
     fi
   done
 }
